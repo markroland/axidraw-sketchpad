02627b77acccf8b088d1fc39610d873b536b16fc
Mark Roland <radiowhir@gmail.com>
2021-02-02 21:37:35 -0800

    Cleaned up code on Negative Space

--- assets/js/patterns/NegativeSpace.js
+++ assets/js/patterns/NegativeSpace.js
@@ -20,7 +20,6 @@ class NegativeSpace {
       );
     }
     triangle.push(triangle[0]);
-    // paths.push(triangle);
 
     // Define function to extract column from multidimensional array
     const arrayColumn = (arr, n) => arr.map(a => a[n]);
@@ -33,43 +32,32 @@ class NegativeSpace {
     let triangle_min_y = Math.min(...y_coordinates)
     let triangle_max_y = Math.max(...y_coordinates)
 
-    // Debuging
-    // console.log(triangle, x_coordinates, triangle_min_x)
-
-    let num_lines = 60;
+    let num_lines = 80;
     let p1,p2
     for(let i = 0; i <= num_lines; i++) {
 
-      // Set intersection values to a maximum
+      // Initialize intersection values
       min_int_x = null;
       max_int_x = null;
 
-      // Left-most Point
+      // Left-most Point of horizontal line
       p1 = [min_x, 2 * (i/num_lines) - 1];
 
-      // Right-most point
+      // Right-most point of horizontal line
       p2 = [max_x, 2 * (i/num_lines) - 1];
 
-      // No intersection betweeen line and triangel, so draw entire line
-      //*
+      // No intersection betweeen line and triangle, so draw entire line
       if (p1[1] > triangle_max_y || p1[1] < triangle_min_y) {
         paths.push([p1,p2]);
         continue;
       }
-      //*/
 
-      // Calculate point of intersection between horizontal line AB
-      // and each side of the triangle
+      // Calculate point of intersection between horizontal line "AB"
+      // and each side of the triangle. Remember, the coordinate system
+      // is the Processing coordinates system (positive is down)
 
+      // Side 1: Betwen 0 and 120 degrees
       let intersect_1 = this.intersect_point(p1,p2,triangle[0],triangle[1]);
-      // if (intersect_1[0] < min_int_x && intersect_1[0] > min_x) {
-      //   min_int_x = intersect_1[0]
-      // }
-      // if (intersect_1[0] > max_int_x && intersect_1[0] < max_x) {
-      //   max_int_x = intersect_1[0]
-      // }
-      // console.log(p1[1], triangle[0][1]);
-      // Remember, coordinate system is the Processing coordinates
       if (p1[1] >= triangle[0][1]) {
         paths.push([
           [intersect_1[0], p1[1]],
@@ -77,20 +65,16 @@ class NegativeSpace {
         ]);
       }
 
-      // Side 2: works
+      // Side 2: Betwen 120 and 240 degrees
       let intersect_2 = this.intersect_point(p1,p2,triangle[1],triangle[2]);
       if (intersect_2[0] < min_int_x) {
-        min_int_x = intersect_2[0]
+        paths.push([
+          p1,
+          [intersect_2[0] , p1[1]]
+        ]);
       }
-      // if (intersect_2[0] > max_int_x) {
-      //   max_int_x = intersect_2[0]
-      // }
-      paths.push([
-        p1,
-        [min_int_x , p1[1]]
-      ]);
 
-      // Side 3
+      // Side 3: Between 240 and 360 degrees
       let intersect_3 = this.intersect_point(p1,p2,triangle[2],triangle[0]);
       if (p1[1] < triangle[0][1]) {
         paths.push([
