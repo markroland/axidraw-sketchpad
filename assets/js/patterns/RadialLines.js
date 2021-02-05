b12155c9e7f1740232f0f9e20f289328315f9495
Mark Roland <radiowhir@gmail.com>
2021-02-05 13:26:31 -0800

    Improved version

--- assets/js/patterns/RadialLines.js
+++ assets/js/patterns/RadialLines.js
@@ -5,39 +5,45 @@ class RadialLines {
     // Create paths array to return
     let paths = new Array();
 
-    let num_lines = 8 * 20;
+    let num_lines = 4 * 40;
 
-    let inner_radius = 0.125;
-    let outter_radius = 2.0;
-
-    let num_rings = 4;
-
-    let radius_increment = (1 - inner_radius) / num_rings;
+    let inner_radius = 0.15;
+    let outer_radius = 2.0;
 
     let radius = 0;
 
     for (let i = 0; i < num_lines; i++) {
 
-        if (i % 8 == 0) {
-            radius = inner_radius + 0 * radius_increment;
-        } else if (i % 4 == 0) {
-            radius = inner_radius + 1 * radius_increment;
-        } else if (i % 2 == 0) {
-            radius = inner_radius + 2 * radius_increment;
-        } else {
-            radius = inner_radius + 3 * radius_increment;
-        }
+      // Note: I'm sure this logic can be improved
 
-        paths.push([
-            [
-                radius * Math.cos(i/num_lines * Math.PI * 2),
-                radius * Math.sin(i/num_lines * Math.PI * 2)
-            ],
-            [
-                outter_radius * Math.cos(i/num_lines * Math.PI * 2),
-                outter_radius * Math.sin(i/num_lines * Math.PI * 2)
-            ]
-        ]);
+      // A
+      radius = inner_radius;
+
+      // D
+      if (i % 2 == 1) {
+        radius = 8 * inner_radius;
+      }
+
+      // C
+      if (i % 8 == 2 || i % 8 == 6) {
+        radius = 4 * inner_radius;
+      }
+
+      // B
+      if (i % 8 == 4) {
+        radius = 2 * inner_radius;
+      }
+
+      paths.push([
+        [
+          radius * Math.cos(i/num_lines * Math.PI * 2),
+          radius * Math.sin(i/num_lines * Math.PI * 2)
+        ],
+        [
+          outer_radius * Math.cos(i/num_lines * Math.PI * 2),
+          outer_radius * Math.sin(i/num_lines * Math.PI * 2)
+        ]
+      ]);
     }
 
     return paths;
