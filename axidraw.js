323ade3bf93ca142cb39a07b47070050375447ac
Mark Roland <radiowhir@gmail.com>
2021-02-04 22:18:40 -0800

    Moved initials

--- axidraw.js
+++ axidraw.js
@@ -55,7 +55,7 @@ let sketch = function(p) {
 
       // Initials
       //*
-      let initials_rotation = "0"; let initials_position = '532,354'; // Landscape
+      let initials_rotation = "0"; let initials_position = '542,354'; // Landscape
       // let initials_rotation = "-90"; let initials_position = '532,30'; // Portrait
       svg_text += '<g transform="translate(' + initials_position + ') rotate(' + initials_rotation + ' 5 5)">'
       svg_text += '<path fill="none" stroke="rgb(0,0,0)" paint-order="fill stroke markers" stroke-opacity="1" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1.42" d="M 0.52831513,9.9326943 2.8794102,-0.05945861 4.0549577,6.1121658 6.6999395,0.52831513 5.5243921,11.108241" id="path1421" /><path fill="none" stroke="rgb(0,0,0)" paint-order="fill stroke markers" stroke-opacity="1" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1.42"  d="m 7.3002589,10.146612 0.458014,-9.61829687 c 0,0 3.7857471,0.3053972 4.1221261,1.83205677 0.336379,1.5266596 -3.2060981,3.6641137 -3.2060981,3.6641137 L 13.712455,10.37562" id="path1423" />';
