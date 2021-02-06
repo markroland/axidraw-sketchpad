9ea44cb3afd54c197e9c0d2e283827f3b380ccc5
Mark Roland <radiowhir@gmail.com>
2021-02-06 08:22:13 -0800

    Extrusion with triangle, square, circle

--- assets/js/patterns/Extrusion.js
+++ assets/js/patterns/Extrusion.js
@@ -6,13 +6,35 @@ class Extrusion {
 
     let PathHelp = new PathHelper();
 
-    let square = PathHelp.polygon(4, 0.3, 0 *Math.PI/4);
-
+    // Triangle
+    let triangle = PathHelp.polygon(3, 0.3, Math.PI/6);
     for (let i = 0; i <= 60; i++) {
       paths.push(
         PathHelp.translatePath(
-          PathHelp.rotatePath(square, i/60 * Math.PI * 2),
+          PathHelp.rotatePath(triangle, i/60 * Math.PI * 2 / 3),
+          [-1.2 + 0.04 * i, -0.7]
+        )
+      );
+    }
+
+    // Square
+    let square = PathHelp.polygon(4, 0.3, Math.PI/4);
+    for (let i = 0; i <= 60; i++) {
+      paths.push(
+        PathHelp.translatePath(
+          PathHelp.rotatePath(square, i/60 * (2 * Math.PI / 4)),
           [-1.2 + 0.04 * i, 0]
+        )
+      );
+    }
+
+    // Circle
+    let circle = PathHelp.polygon(60, 0.3, 0);
+    for (let i = 0; i <= 60; i++) {
+      paths.push(
+        PathHelp.translatePath(
+          PathHelp.rotatePath(circle, i/60 * (2 * Math.PI / 4)),
+          [-1.2 + 0.04 * i, 0.7]
         )
       );
     }
