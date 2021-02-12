c3b80658040fad97d00aede54f0c586ca674c385
Mark Roland <radiowhir@gmail.com>
2021-02-12 09:05:40 -0800

    Connected grid shapes with randomness

--- assets/js/patterns/Grid.js
+++ assets/js/patterns/Grid.js
@@ -26,6 +26,7 @@ class Grid {
     let PathHelp = new PathHelper();
 
     let grid_points = new Array();
+
     let path = new Array();
     for (let a = 0; a <= a_max; a++) {
       for (let b = 0; b <= b_max; b++) {
@@ -34,10 +35,10 @@ class Grid {
           grid_points[a] = new Array();
         }
         grid_points[a][b] = [
-          a * side_length + PathHelp.getRandom(-0.1, 0.1),
-          b * side_length + PathHelp.getRandom(-0.1, 0.1)
+          PathHelp.getRandom(-0.15, 0.15),
+          PathHelp.getRandom(-0.15, 0.15)
         ]
-        path.push([grid_points[a][b][0],grid_points[a][b][1]])
+        // path.push([grid_points[a][b][0],grid_points[a][b][1]])
       }
     }
 
@@ -49,46 +50,88 @@ class Grid {
 
       for (let b = 0; b < b_max; b++) {
 
-        // Test shape ("side_length" is actually the side length in this case)
-        /*
+        // 4-sided closed polygon
         let base_shape = [
-          [-side_length/2, side_length/2],
-          [side_length/2, side_length/2],
-          [side_length/2, -side_length/2],
-          [-side_length/2, -side_length/2],
-          [-side_length/2, side_length/2]
-        ];
-        //*/
-
-        let base_shape = [
-          [grid_points[a][b][0], grid_points[a][b][1]],
-          [grid_points[a+1][b][0], grid_points[a+1][b][1]],
-          [grid_points[a+1][b+1][0], grid_points[a+1][b+1][1]],
-          [grid_points[a][b+1][0], grid_points[a][b+1][1]],
-          [grid_points[a][b][0], grid_points[a][b][1]],
+          [-side_length/2 + grid_points[a][b][0],   -side_length/2 + grid_points[a][b][1]],
+          [ side_length/2 + grid_points[a+1][b][0],   -side_length/2 + grid_points[a+1][b][1]],
+          [ side_length/2 + grid_points[a+1][b+1][0],    side_length/2 + grid_points[a+1][b+1][1]],
+          [-side_length/2 + grid_points[a][b+1][0],    side_length/2 + grid_points[a][b+1][1]],
+          [-side_length/2 + grid_points[a][b][0],   -side_length/2 + grid_points[a][b][1]],
         ]
 
         // Scale Shape
-        let scaled_shape = PathHelp.scalePath(base_shape, 1.0)
+        let scaled_shape = PathHelp.scalePath(base_shape, 0.9)
 
         // Individual shape Translate
-        // let translated_shape = scaled_shape;
-        /*
         let translated_shape = PathHelp.translatePath(
             scaled_shape,
             [2 * (a_max/b_max) * (a/a_max), 2 * (b/b_max)]
         );
+
+        // Subdivide the translated path so that Bezier control points can be extracted
+        let sub_shape = PathHelp.subdividePath(translated_shape);
+
+        // Build a new shape composed of Quadratic Bezier curves
+        let num_verts = 10;
+        let shape = new Array();
+
+        // Build "manually"
+        /*
+
+        // Corner 1
+        let quadratic_bezier = PathHelp.quadraticBezierPath(
+          sub_shape[1],
+          sub_shape[2],
+          sub_shape[3],
+          num_verts
+        )
+        shape = shape.concat(quadratic_bezier);
+
+        // Corner 2
+        quadratic_bezier = PathHelp.quadraticBezierPath(
+          sub_shape[3],
+          sub_shape[4],
+          sub_shape[5],
+          num_verts
+        )
+        shape = shape.concat(quadratic_bezier.slice(-num_verts));
+
+        // Corner 3
+        quadratic_bezier = PathHelp.quadraticBezierPath(
+          sub_shape[5],
+          sub_shape[6],
+          sub_shape[7],
+          num_verts
+        )
+        shape = shape.concat(quadratic_bezier.slice(-num_verts));
+
+        // Corner 4
+        quadratic_bezier = PathHelp.quadraticBezierPath(
+          sub_shape[7],
+          sub_shape[0],
+          sub_shape[1],
+          num_verts
+        )
+        shape = shape.concat(quadratic_bezier.slice(-num_verts));
+
         //*/
 
-        //*
-        let translated_shape = PathHelp.translatePath(
-            scaled_shape,
-            [-(5/3)/4, -(2/3)/2]
-        );
-        //*/
+        // Incomplete generalization of corners
+        // Warning: Not tested for shapes with more than 4 corners
+        // Warning: Some points may be duplicated!
+        let bezier_path = new Array();
+        for (let s = 0; s < base_shape.length - 1; s++) {
+          bezier_path = PathHelp.quadraticBezierPath(
+            sub_shape[((s*2) + 1) % sub_shape.length],
+            sub_shape[((s*2) + 2) % sub_shape.length],
+            sub_shape[((s*2) + 3) % sub_shape.length],
+            num_verts
+          )
+          shape = shape.concat(bezier_path);
+        }
 
-        // Add to paths array
-        paths.push(translated_shape);
+        // Add shape to paths array
+        paths.push(shape);
       }
     }
 
