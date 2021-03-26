a46b128698cd80103749240cd3ec7b3ce85c0ea1
Mark Roland <radiowhir@gmail.com>
2021-03-25 22:46:58 -0700

    Plotting improvements to Cassini Oval

--- assets/js/patterns/CassiniOval.js
+++ assets/js/patterns/CassiniOval.js
@@ -35,39 +35,55 @@ class CassiniOval {
     let a = 0.6;
     let b = 1;
     let r = 1;
-    let increment = 0.02
+    let increment = 0.1
     let sign = new Array();
 
     // b/a < 1
     //*
-    sign = [
-      [-1, -1],
-      [-1, +1],
-      [+1, -1],
-      [+1, +1]
+    // Set the +/- signs. Left lobe is first, Right lobe is second
+    let lobe_signs = [
+      [
+        [-1, -1],
+        [-1, +1]
+      ],
+      [
+        [+1, -1],
+        [+1, +1]
+      ]
     ]
-    for (let s = 0; s < sign.length; s++) {
+    for (let lobe = 0; lobe < 2; lobe++) {
+      let sign = lobe_signs[lobe]
       for (b = 0.2; b < a; b += increment) {
         path = Array();
-        for (let i = -0.5 * segments; i <= 0.5 * segments; i++) {
-          let theta = (i/(0.5 * segments)) * 0.5 * Math.asin(Math.pow(b/a,2))
+        for (let s = 0; s < sign.length; s++) {
+          for (let i = -0.5 * segments; i < 0.5 * segments; i++) {
+            let theta = (i/(0.5 * segments)) * 0.5 * Math.asin(Math.pow(b/a,2))
 
-          // For values like b = 0.4 there is a floating-point math problem
-          // in the form of negative exponents close to zero. This is solved
-          // here by rounding everything under a threshold to zero.
-          let radicand = Math.pow(b/a, 4) - Math.pow(Math.sin(n * theta),2)
-          radicand = radicand < 0.000001 ? 0.0 : radicand
+            // Reverse the direction of Theta in order to stich the lines together continuously
+            if (s % 2 == 1) {
+              theta = -1 * theta
+            }
 
-          r = sign[s][0] * a * Math.pow(
-            Math.cos(n * theta)
-            + sign[s][1] * Math.sqrt(radicand),
-            1/n
-          );
+            // For values like b = 0.4 there is a floating-point math problem
+            // in the form of negative exponents close to zero. This is solved
+            // here by rounding everything under a threshold to zero.
+            let radicand = Math.pow(b/a, 4) - Math.pow(Math.sin(n * theta),2)
+            radicand = radicand < 0.000001 ? 0.0 : radicand
 
-          let coordinates = PathHelp.polarToRect(r, theta);
+            r = sign[s][0] * a * Math.pow(
+              Math.cos(n * theta)
+              + sign[s][1] * Math.sqrt(radicand),
+              1/n
+            );
 
-          path.push(coordinates)
+            let coordinates = PathHelp.polarToRect(r, theta);
+
+            path.push(coordinates)
+          }
         }
+        // Close the shape with the first point
+        path.push(path[0])
+
         paths.push(path)
       }
     }
@@ -75,16 +91,23 @@ class CassiniOval {
 
     // Lemniscate condition, b/a = 1
     // Other sign conditions yield imaginary numbers
+    // Draw as a single continuous path
     //*
     b = a
     sign = [
       [-1, +1],
       [+1, +1]
     ]
+    path = Array();
     for (let s = 0; s < sign.length; s++) {
-      path = Array();
-      for (let i = -0.5 * segments; i <= 0.5 * segments; i++) {
+      for (let i = -0.5 * segments; i < 0.5 * segments; i++) {
         let theta = (i/(0.5 * segments)) * 0.5 * Math.asin(Math.pow(b/a,2))
+
+        // Reverse theta for 2nd, right lobe to draw a continuous line
+        if (s == 1) {
+          theta = -1 * theta;
+        }
+
         r = sign[s][0] * a * Math.pow(
           Math.cos(n * theta) + sign[s][1] * Math.sqrt(Math.pow(b/a, 4) - Math.pow(Math.sin(n * theta),2)),
           1/n
@@ -93,8 +116,12 @@ class CassiniOval {
         // coordinates = coordinates.map(x => x < 0.000001 ? 0.0 : x)
         path.push(coordinates)
       }
-      paths.push(path)
     }
+
+    // Close the shape with the first point
+    path.push(path[0])
+
+    paths.push(path)
     //*/
 
     // b/a > 1
@@ -102,7 +129,7 @@ class CassiniOval {
     for (b = a + increment; b < 2 * a; b += increment) {
       path = Array();
       for (let i = -0.5 * segments; i <= 0.5 * segments; i++) {
-        let theta = (i/segments) * Math.PI * 2
+        let theta = ((i/segments) * Math.PI * 2) + Math.PI/2
         r = a * Math.pow(
           Math.cos(n * theta) + Math.sqrt(
             Math.pow(b/a, 4) - Math.pow(Math.sin(n * theta),2)
@@ -112,6 +139,10 @@ class CassiniOval {
         let coordinates = PathHelp.polarToRect(r, theta);
         path.push(coordinates)
       }
+
+      // Add some more points so the pen up position goes beyond the pen down position
+      path = path.concat(path.slice(0,10))
+
       paths.push(path)
     }
     //*/
