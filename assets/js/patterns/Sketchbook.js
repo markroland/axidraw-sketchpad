6b4146df575cee7eb395fc9c7159c24113e32d4a
Mark Roland <radiowhir@gmail.com>
2021-05-04 22:40:13 -0700

    Polygon clipping proof-of-concept

--- assets/js/patterns/Sketchbook.js
+++ assets/js/patterns/Sketchbook.js
@@ -10,13 +10,16 @@ class Sketchbook {
     this.name = "Sketchbook";
 
     this.constrain = false
+
+    this.title = "Toxiclibs test"
   }
 
   /**
    * Draw path
    */
   draw(p5) {
-    return this.default()
+    // return this.default()
+    return this.toxiclibtest()
   }
 
   default() {
@@ -37,5 +40,73 @@ class Sketchbook {
     })
 
     return layers;
+  }
+
+  /*
+   * http://haptic-data.com/toxiclibsjs/examples/polygon-clipping-p5
+   * http://toxiclibs.org/docs/core/
+   * https://en.wikipedia.org/wiki/Sutherland–Hodgman_algorithm
+   **/
+  toxiclibtest() {
+
+    let PathHelp = new PathHelper;
+
+    let layers = new Array();
+
+    let paths = new Array();
+
+    let path = new Array();
+
+    // Circle 1
+    let circle1 = new toxi.geom.Circle(1.0);
+    circle1.x = 0.5
+    let polyCircle1 = circle1.toPolygon2D(120);
+
+    // Convert to Polyline
+    path = new Array();
+    for (let point of polyCircle1.vertices) {
+      path.push([point.x, point.y])
+    }
+    path.push(path[0])
+    paths.push(path);
+
+    // Circle 2
+    let circle2 = new toxi.geom.Circle(1.0);
+    circle2.x = -0.5
+    let polyCircle2 = circle2.toPolygon2D(120);
+
+    // Convert to Polyline
+    /*
+    path = new Array();
+    for (let point of polyCircle2.vertices) {
+      path.push([point.x, point.y])
+    }
+    path.push(path[0])
+    paths.push(path);
+    */
+
+    // Use rectangle to clip Circle 2
+    var clipper = new toxi.geom.SutherlandHodgemanClipper(
+      new toxi.geom.Rect(-2,0,4,0.5)
+    );
+    let clippedShape = clipper.clipPolygon(polyCircle2);
+    console.log(clippedShape);
+
+    // Convert to Polyline
+    path = new Array();
+    for (let point of clippedShape.vertices) {
+      path.push([point.x, point.y])
+    }
+    path.push(path[0])
+    paths.push(path);
+
+    // Add paths to layer
+    layers.push({
+      "color": "black",
+      "paths": paths
+    })
+
+    return layers;
+
   }
 }
