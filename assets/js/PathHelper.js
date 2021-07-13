886664d65f0df9d8143edbae64ccca1cb131a60f
Mark Roland <radiowhir@gmail.com>
2021-07-12 21:30:56 -0700

    Porsche 959 - Concave Hull Intersection Study

--- assets/js/PathHelper.js
+++ assets/js/PathHelper.js
@@ -120,6 +120,59 @@ class PathHelper {
     return [x, y]
   }
 
+  /**
+   * Calculate if and where two finite line segments intersect
+   * From https://stackoverflow.com/a/30159167
+   * @param Array A point array containing two values for x and y. Start Point of Line A
+   * @param Array A point array containing two values for x and y. End Point of Line A
+   * @param Array A point array containing two values for x and y. Start Point of Line B
+   * @param Array A point array containing two values for x and y. End Point of Line B
+   * @return Boolean True if the lines intersect, false otherwise
+   */
+  getLineLineCollision(p0, p1, p2, p3) {
+
+    var s1, s2;
+    s1 = {x: p1.x - p0.x, y: p1.y - p0.y};
+    s2 = {x: p3.x - p2.x, y: p3.y - p2.y};
+
+    var s10_x = p1.x - p0.x;
+    var s10_y = p1.y - p0.y;
+    var s32_x = p3.x - p2.x;
+    var s32_y = p3.y - p2.y;
+
+    var denom = s10_x * s32_y - s32_x * s10_y;
+
+    if(denom == 0) {
+        return false;
+    }
+
+    var denom_positive = denom > 0;
+
+    var s02_x = p0.x - p2.x;
+    var s02_y = p0.y - p2.y;
+
+    var s_numer = s10_x * s02_y - s10_y * s02_x;
+
+    if((s_numer < 0) == denom_positive) {
+        return false;
+    }
+
+    var t_numer = s32_x * s02_y - s32_y * s02_x;
+
+    if((t_numer < 0) == denom_positive) {
+        return false;
+    }
+
+    if((s_numer > denom) == denom_positive || (t_numer > denom) == denom_positive) {
+        return false;
+    }
+
+    var t = t_numer / denom;
+
+    var p = {x: p0.x + (t * s10_x), y: p0.y + (t * s10_y)};
+    return p;
+  }
+
   distance(p1, p2) {
     return Math.sqrt(Math.pow(p2[0] - p1[0], 2) + Math.pow(p2[1] - p1[1], 2))
   }
